2d1204c0ce7a6d3f54c03bd2f3c90dad8df98ecd
marekwietecki <marekwietecki10@gmail.com>
2025-05-22 18:16:57 +0200

    supabase-client added

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,10 +1,25 @@
 import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="home" options={{ tabBarLabel: "Home", title: "Home" }} />
-      <Tabs.Screen name="settings" options={{ tabBarLabel: "Settings", title: "Settings" }} />
+      <Tabs.Screen name="home" options={{ tabBarLabel: "Home", title: "Home", tabBarIcon: ({ color }) => (
+        <FontAwesome
+          size={28}
+          style={{marginBottom: -3}}
+          name="home"
+          color={color}
+        />
+      ) }}  />
+      <Tabs.Screen name="settings" options={{ tabBarLabel: "Settings", title: "Settings" , tabBarIcon: ({ color }) => (
+        <FontAwesome
+          size={28}
+          style={{marginBottom: -3}}
+          name="gear"
+          color={color}
+        />
+      ) }} />
     </Tabs>
   );
 }
